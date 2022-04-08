11944db3019c61b36d80a782d58b2e793822fe9b
SÜLEYMAN YİĞİT GÜMEN <suleymanyigit.gumen@stu.bahcesehir.k12.tr>
2022-04-08 14:35:58 +0300

    Add project files.

---
+++ Infrastructure/ETicaretAPI.Persistence/Concretes/ProductService.cs
@@ -0,0 +1,23 @@
+﻿using ETicaretAPI.Application.Abstraction;
+using ETicaretAPI.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Persistence.Concretes
+{
+    public class ProductService : IProductService
+    {
+        public List<Product> GetProducts()
+        => new()
+        {
+            new() { Id = Guid.NewGuid(), Name = "Ürün 1", Price = 100, Stock = 10 },
+            new() { Id = Guid.NewGuid(), Name = "Ürün 2", Price = 200, Stock = 10 },
+            new() { Id = Guid.NewGuid(), Name = "Ürün 3", Price = 300, Stock = 10 },
+            new() { Id = Guid.NewGuid(), Name = "Ürün 4", Price = 400, Stock = 10 },
+            new() { Id = Guid.NewGuid(), Name = "Ürün 5", Price = 500, Stock = 10 },
+        };
+    }
+}
---
+++ Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
@@ -0,0 +1,30 @@
+﻿using ETicaretAPI.Application.Abstraction;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            var result = _productService.GetProducts();
+            return Ok(result);
+        }
+    }
+}
